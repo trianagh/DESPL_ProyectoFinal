436fde29b3adca88904fc619c758cc5961e73022
trianagh <trianagh@gmail.com>
2022-02-18 19:02:21 +0100

    arreglado
    arreglado finalmente

---
+++ DIW - Proyecto final/js/scripts.js
@@ -0,0 +1,59 @@
+function suma() {
+
+    var gv = 2.99;
+    var total=0; 
+    var productos=0;  
+   
+    
+    $(".bolsos tr").each(function () {
+        if (!$(this).hasClass("fila_princ")) {
+            total += parseFloat($(this).find(".producto").text());
+            productos++;  
+               
+        }      
+
+    });
+    
+    preciofinal=(total+gv).toFixed(2);
+    $(".total").text(preciofinal);
+    $(".num_cart").text(productos);
+
+    if(total==0){
+        $(".total").text("No hay productos");
+        $(".gastos_envio").hide();         
+    }
+
+
+}
+
+$(document).ready(function () {
+    suma();
+    $(".eliminar1").click(function () {
+        $(".fila1").remove();
+        suma();
+    });
+
+    $(".eliminar2").click(function () {
+        $(".fila2").remove();
+        suma();
+    });
+    $(document).scroll(function () {
+        
+        if ($(this).scrollTop() > 0) {
+            $('#totop').fadeIn(500);
+        } else {
+            $('#totop').fadeOut(500);
+        }
+    });
+    $('#totop').click(function () {
+        
+        $("html, body").animate({ scrollTop: 0 }, 600);
+    });
+    
+});
+
+
+
+
+
+
